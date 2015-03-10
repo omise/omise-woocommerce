ec5c599807b9860c6e003a9969a25b4fd1954091
Tommy Omise <tommy@omise.co>
2015-03-10 17:48:08 +0700

    Bump version to 1.0.1

--- omise-gateway.php
+++ omise-gateway.php
@@ -14,7 +14,7 @@ defined('ABSPATH') or die("No direct script access allowed.");
 define("OMISE_PROTOCOL_PREFIX", "https://");
 define("OMISE_VAULT_HOST", "vault.omise.co");
 define("OMISE_API_HOST", "api.omise.co");
-define("OMISE_WOOCOMMERCE_PLUGIN_VERSION", "1.0");
+define("OMISE_WOOCOMMERCE_PLUGIN_VERSION", "1.0.1");
 
 require_once 'omise-util.php';
 require_once 'omise-api-wrapper.php';
