151094fa0bbb72e9d2ff0d546e37a36446d44110
zkan <kan@prontomarketing.com>
2016-02-12 21:38:35 +0700

    Add tests to check each server variable used to get client IP

--- tests/test-omise-util.php
+++ tests/test-omise-util.php
@@ -8,6 +8,15 @@ class Omise_Util_Test extends WP_UnitTestCase {
         $this->expected = "192.168.1.1";
     }
 
+    public function tearDown() {
+        $_SERVER["HTTP_CLIENT_IP"]       = "";
+        $_SERVER["HTTP_X_FORWARDED_FOR"] = "";
+        $_SERVER["HTTP_X_FORWARDED"]     = "";
+        $_SERVER["HTTP_FORWARDED_FOR"]   = "";
+        $_SERVER["HTTP_FORWARDED"]       = "";
+        $_SERVER["REMOTE_ADDR"]          = "";
+    }
+
     function test_get_client_ip_should_return_ip_from_http_client_ip_if_set() {
         $_SERVER["HTTP_CLIENT_IP"] = "192.168.1.1";
         $ip = Omise_Util::get_client_ip();
@@ -16,6 +25,36 @@ class Omise_Util_Test extends WP_UnitTestCase {
 
     function test_get_client_ip_should_return_ip_from_http_x_forwarded_for_if_set() {
         $_SERVER["HTTP_X_FORWARDED_FOR"] = "192.168.1.1";
+        $ip = Omise_Util::get_client_ip();
+        $this->assertEquals( $this->expected, $ip );
+    }
+
+    function test_get_client_ip_should_return_ip_from_http_x_forwarded_if_set() {
+        $_SERVER["HTTP_X_FORWARDED"] = "192.168.1.1";
+        $ip = Omise_Util::get_client_ip();
+        $this->assertEquals( $this->expected, $ip );
+    }
+
+    function test_get_client_ip_should_return_ip_from_http_forwarded_for_if_set() {
+        $_SERVER["HTTP_FORWARDED_FOR"] = "192.168.1.1";
+        $ip = Omise_Util::get_client_ip();
+        $this->assertEquals( $this->expected, $ip );
+    }
+
+    function test_get_client_ip_should_return_ip_from_http_forwarded_if_set() {
+        $_SERVER["HTTP_FORWARDED"] = "192.168.1.1";
+        $ip = Omise_Util::get_client_ip();
+        $this->assertEquals( $this->expected, $ip );
+    }
+
+    function test_get_client_ip_should_return_ip_from_remote_addr_if_set() {
+        $_SERVER["REMOTE_ADDR"] = "192.168.1.1";
+        $ip = Omise_Util::get_client_ip();
+        $this->assertEquals( $this->expected, $ip );
+    }
+
+    function test_get_client_ip_should_return_unknown_if_server_variable_is_not_set() {
+        $this->expected = "UNKNOWN";
         $ip = Omise_Util::get_client_ip();
         $this->assertEquals( $this->expected, $ip );
     }
