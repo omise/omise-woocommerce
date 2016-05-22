e5786f3486da434b3cb8bd9ec5e64cb8a87fa0db
guzzilar <iam.nuttanon@gmail.com>
2016-05-23 04:30:22 +0700

    Reorder Omise key fields and make secret key fields to be hidden from the screen

--- omise-wc-gateway.php
+++ omise-wc-gateway.php
@@ -81,26 +81,26 @@ function register_omise_wc_gateway_plugin() {
 								'description' => __ ( 'This controls the description which the user sees during checkout.', $this->gateway_name ),
 								'default' => __ ( 'Omise payment gateway.', $this->gateway_name ) 
 						),
-						'test_private_key' => array (
-								'title' => __ ( 'Secret key for test', $this->gateway_name ),
-								'type' => 'text',
-								'description' => __ ( 'The "Test" mode secret key which can be found in Omise Dashboard"' ) 
-						),
 						'test_public_key' => array (
 								'title' => __ ( 'Public key for test', $this->gateway_name ),
 								'type' => 'text',
-								'description' => __ ( 'The "Test" mode public key which can be found in Omise Dashboard"' ) 
+								'description' => __ ( 'The "Test" mode public key which can be found in Omise Dashboard' ) 
 						),
-						'live_private_key' => array (
-								'title' => __ ( 'Secret key for live', $this->gateway_name ),
-								'type' => 'text',
-								'description' => __ ( 'The "Live" mode secret key which can be found in Omise Dashboard"' ) 
+						'test_private_key' => array (
+								'title' => __ ( 'Secret key for test', $this->gateway_name ),
+								'type' => 'password',
+								'description' => __ ( 'The "Test" mode secret key which can be found in Omise Dashboard' ) 
 						),
 						'live_public_key' => array (
 								'title' => __ ( 'Public key for live', $this->gateway_name ),
 								'type' => 'text',
-								'description' => __ ( 'The "Live" mode public key which can be found in Omise Dashboard"' ) 
-						) 
+								'description' => __ ( 'The "Live" mode public key which can be found in Omise Dashboard' ) 
+						),
+						'live_private_key' => array (
+								'title' => __ ( 'Secret key for live', $this->gateway_name ),
+								'type' => 'password',
+								'description' => __ ( 'The "Live" mode secret key which can be found in Omise Dashboard' ) 
+						)
 				);
 			}
 			
