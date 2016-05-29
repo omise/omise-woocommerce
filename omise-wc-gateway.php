90db27038f66939d7eaf4369103523da98ac7fd6
guzzilar <iam.nuttanon@gmail.com>
2016-05-30 05:53:31 +0700

    Enhance and clean code, fix bugs, correct the order status after authorize success

--- omise-wc-gateway.php
+++ omise-wc-gateway.php
@@ -32,7 +32,7 @@ function register_omise_wc_gateway_plugin() {
 				$this->public_key       = $this->sandbox ? $this->test_public_key : $this->live_public_key;
 				$this->private_key      = $this->sandbox ? $this->test_private_key : $this->live_private_key;
 
-				add_action( 'woocommerce_api_wc_gateway_' . $this->id, array( $this, 'omise_3ds_handler' ) );
+				add_action( 'woocommerce_api_wc_gateway_' . $this->id, array( $this, 'omise_3ds_callback' ) );
 				add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( &$this, 'process_admin_options' ) );
 				add_action( 'wp_enqueue_scripts', array( $this, 'omise_scripts' ) );
 			}
@@ -147,7 +147,7 @@ function register_omise_wc_gateway_plugin() {
 			 * 
 			 * @see WC_Payment_Gateway::process_payment()
 			 */
-			public function process_payment( $order_id ) {				
+			public function process_payment( $order_id ) {
 				$order   = wc_get_order( $order_id );
 				$token   = isset( $_POST['omise_token'] ) ? wc_clean( $_POST['omise_token'] ) : '';
 				$card_id = isset( $_POST['card_id'] ) ? wc_clean( $_POST['card_id'] ) : '';
@@ -160,43 +160,43 @@ function register_omise_wc_gateway_plugin() {
 					$omise_customer_id = $this->sandbox ? $user->test_omise_customer_id : $user->live_omise_customer_id;
 
 					if ( isset( $_POST['omise_save_customer_card'] ) && empty( $card_id ) ) {
-						if (empty($token)){
-							throw new Exception ( "Omise card token is required." );
+						if ( empty( $token ) ) {
+							throw new Exception( "Omise card token is required." );
 						}
 
-						if (! empty ( $omise_customer_id )) {
+						if ( ! empty ( $omise_customer_id ) ) {
 							// attach a new card to customer
-							$omise_customer = Omise::create_card ( $this->private_key, $omise_customer_id, $token );
+							$omise_customer = Omise::create_card( $this->private_key, $omise_customer_id, $token );
 
-							if($omise_customer->object=="error"){
-								throw new Exception($omise_customer->message);
+							if ( $omise_customer->object == "error" ) {
+								throw new Exception( $omise_customer->message );
 							}
 
-							$card_id = $omise_customer->cards->data [$omise_customer->cards->total - 1]->id;
+							$card_id = $omise_customer->cards->data[$omise_customer->cards->total - 1]->id;
 						} else {
-							$description = "WooCommerce customer " . $user->id;
-							$customer_data = array (
-									"description" => $description,
-									"card" => $token 
+							$description   = "WooCommerce customer " . $user->id;
+							$customer_data = array(
+								"description" => $description,
+								"card"        => $token
 							);
 
-							$omise_customer = Omise::create_customer ( $this->private_key, $customer_data );
+							$omise_customer = Omise::create_customer( $this->private_key, $customer_data );
 
-							if($omise_customer->object=="error"){
-								throw new Exception($omise_customer->message);
+							if ( $omise_customer->object == "error" ) {
+								throw new Exception( $omise_customer->message );
 							}
 
 							$omise_customer_id = $omise_customer->id;
-							if($this->sandbox){
-								update_user_meta ( $user->ID, 'test_omise_customer_id', $omise_customer_id );
+							if( $this->sandbox ) {
+								update_user_meta( $user->ID, 'test_omise_customer_id', $omise_customer_id );
 							}else{
-								update_user_meta ( $user->ID, 'live_omise_customer_id', $omise_customer_id );
+								update_user_meta( $user->ID, 'live_omise_customer_id', $omise_customer_id );
 							}
 
-							if (0 == sizeof ( $omise_customer->cards->data )) {
-								throw new Exception ( "Something wrong with Omise gateway. No card available for creating a charge." );
+							if ( 0 == sizeof( $omise_customer->cards->data ) ) {
+								throw new Exception( "Something wrong with Omise gateway. No card available for creating a charge." );
 							}
-							$card = $omise_customer->cards->data [0]; //use the latest card
+							$card    = $omise_customer->cards->data [0]; //use the latest card
 							$card_id = $card->id;
 						}
 					}
@@ -208,98 +208,169 @@ function register_omise_wc_gateway_plugin() {
 					else
 						$amount = $order->get_total();
 
-					$data = array (
+					$data = array(
 						"amount"      => $amount,
 						"currency"    => $order_currency,
 						"description" => "WooCommerce Order id " . $order_id,
 						"return_uri"  => add_query_arg( 'order_id', $order_id, site_url()."?wc-api=wc_gateway_omise" )
 					);
 					
-					if (! empty ( $card_id ) && ! empty ( $omise_customer_id )) {
+					if ( ! empty( $card_id ) && ! empty( $omise_customer_id ) ) {
 						// create charge with a specific card of customer
-						$data["customer"] =  $omise_customer_id;
-						$data["card"] = $card_id;
-					} else if (! empty ( $token )) {
+						$data["customer"] = $omise_customer_id;
+						$data["card"]     = $card_id;
+					} else if ( ! empty( $token ) ) {
 						$data["card"] = $token;
 					} else {
 						throw new Exception ( "Please select a card or enter new payment information." );
 					}
 
 					// Auto capture or not?
-					if ( "auto_capture" === $this->payment_action ) {
-						$data['capture'] = true;
-					} else if ( "manual_capture" === $this->payment_action ) {
-						$data['capture'] = false;
-					}
+					if ( 'auto_capture' === $this->payment_action )
+						$result = $this->process_charge_authorize_and_capture( $this->private_key, $data );
 
-					$result = Omise::create_charge($this->private_key, $data);
-					if ($result->object === "error") {
-						throw new Exception ($result->message);
-					} if ($result->failure_code) {
-						throw new Exception ($result->failure_message);
-					} else {
-						$post_id = wp_insert_post(array(
-							'post_title'	=> 'Omise Charge Id '.$result->id,
-							'post_type'		=> 'omise_charge_items',
-							'post_status'	=> 'publish'
-						));
+					if ( 'manual_capture' === $this->payment_action )
+						$result = $this->process_charge_authorize( $this->private_key, $data );
 
-						add_post_meta($post_id, '_omise_charge_id', $result->id);
-						add_post_meta($post_id, '_wc_order_id', $order_id);
-						add_post_meta($post_id, '_wc_confirmed_url', $this->get_return_url($order));
-					}
+					if ( ! isset( $result ) )
+						throw new Exception( 'Method not found' );
 
-					$success = false;
-					if ($this->omise_3ds) {
+					if ( is_array( $result ) && isset( $result['object'] ) && 'error' === $result['object'] )
+						throw new Exception( $result['message'] );
+
+					// Register new post
+					$this->register_omise_charge_post( $result, $order, $order );
+
+					// Check 3D-Secure enabled
+					if ( $this->omise_3ds ) {
 						return array (
-							'result' => 'success',
+							'result'   => 'success',
 							'redirect' => $result->authorize_uri
 						);
 					} else {
 						// Auto capture or not?
-						if ( "auto_capture" === $this->payment_action ) {
-							$success = $this->is_charge_success( $result );
-						} else if ( "manual_capture" === $this->payment_action ) {
-							$success = $this->is_authorized_success( $result );
+						if ( 'auto_capture' === $this->payment_action ) {
+							$success = $this->is_charge_paid( $result );
+							if ( $success ) {
+								$order->payment_complete();
+								$order->add_order_note( 'Payment with Omise successful' );
+							}
 						}
-					}
 
-					if ($success) {
-						$order->payment_complete ();
-						$order->add_order_note ( 'Payment with Omise successful' );
+						if ( 'manual_capture' === $this->payment_action ) {
+							$success = $this->is_charge_authorized( $result );
+							if ( $success ) {
+								$order->add_order_note( 'Authorize with Omise successful' );
+							}
+						}
+
+						if ( ! $success )
+							throw new Exception( $this->get_charge_error_message( $result ) );
+
 						// Remove cart
 						WC()->cart->empty_cart();
 						return array (
-								'result' => 'success',
-								'redirect' => $this->get_return_url ( $order ) 
+							'result'   => 'success',
+							'redirect' => $this->get_return_url( $order )
 						);
-					} else {
-						throw new Exception($this->get_charge_error_message($result));
 					}
-				}
-				catch( Exception $e ){
+				} catch( Exception $e ) {
 					$error_message = $e->getMessage();
-					wc_add_notice( __('Payment error:', 'woothemes') . $error_message , 'error' );
-					$order->add_order_note ( 'Payment with Omise error : '. $error_message );
+					wc_add_notice( __( 'Payment error:', 'woothemes' ) . $error_message , 'error' );
+					$order->add_order_note( 'Payment with Omise error : '. $error_message );
 					return array(
-							'result'   => 'fail',
-							'redirect' => ''
+						'result'   => 'fail',
+						'redirect' => ''
 					);
 				}
 			}
 
-			private function is_authorized_success( $result ) {
-				if ( ! isset( $result->object ) || 'charge' !== $result->object )
-				    return false;
+			private function process_charge_authorize( $omise_skey, $data ) {
+				$data['capture'] = false;
 
-				if ( true === $charge['authorized'] )
-				    return true;
+				try {
+					$charge = Omise::create_charge( $omise_skey, $data );
+
+					if ( $charge->object === 'error' )
+						throw new Exception( $result->message );
+
+					if ( $charge->failure_code )
+						throw new Exception( $result->failure_message );
+
+				} catch ( Exception $e ) {
+					$charge = array(
+						'object'  => 'error',
+						'message' => $e->getMessage()
+					);
+				}
+
+				return $charge;
+			}
+
+			private function process_charge_authorize_and_capture( $omise_skey, $data ) {
+				$data['capture'] = true;
+
+				try {
+					$charge = Omise::create_charge( $omise_skey, $data );
+
+					if ( $charge->object === 'error' )
+						throw new Exception( $result->message );
+
+					if ( $charge->failure_code )
+						throw new Exception( $result->failure_message );
+
+				} catch ( Exception $e ) {
+					$charge = array(
+						'object'  => 'error',
+						'message' => $e->getMessage()
+					);
+				}
+
+				return $charge;
+			}
+
+			private function register_omise_charge_post( $result, $order, $order_id ) {
+				$post_id = wp_insert_post(
+					array(
+						'post_title'  => 'Omise Charge Id ' . $result->id,
+						'post_type'   => 'omise_charge_items',
+						'post_status' => 'publish'
+					)
+				);
+
+				add_post_meta( $post_id, '_omise_charge_id', $result->id );
+				add_post_meta( $post_id, '_wc_order_id', $order_id );
+				add_post_meta( $post_id, '_wc_confirmed_url', $this->get_return_url( $order ) );
+			}
+
+			/**
+			 * @param object $result
+			 * @return boolean
+			 */
+			private function is_charge_authorized( $result ) {
+				if ( ! isset( $result->object ) || 'charge' !== $result->object )
+					return false;
+
+				if ( true == $result->authorized )
+					return true;
 
 				return false;
 			}
 
-			private function is_charge_success($result){
-				return isset ( $result->id ) && isset( $result->object ) && $result->object == 'charge' && $result->captured == true;
+			/**
+			 * @param object $result
+			 * @return boolean
+			 */
+			private function is_charge_paid( $result ) {
+				if ( ! isset( $result->object ) || 'charge' !== $result->object )
+					return false;
+
+				// support Omise API version '2014-07-27' by checking if 'captured' exist.
+				$paid = isset( $result->captured ) ? $result->captured : $result->paid;
+				if ( isset( $result->id ) && true === $paid )
+					return true;
+
+				return false;
 			}
 
 			private function get_charge_error_message($result){
@@ -354,53 +425,63 @@ function register_omise_wc_gateway_plugin() {
 				) );
 			}
 
-			public function omise_3ds_handler()
-			{
-				if (!$_GET['order_id'])
+			public function omise_3ds_callback() {
+				if ( ! $_GET['order_id'] )
 					wp_die( "Order was not found", "Omise Payment Gateway: Checkout", array( 'response' => 500 ) );
 
-				$order_id 	= $_GET['order_id'];
+				$order_id = $_GET['order_id'];
+				$posts    = get_posts(array(
+					'post_type'   => 'omise_charge_items',
+					'meta_query'  => array( array(
+						'key'     => '_wc_order_id',
+						'value'   => $order_id,
+						'compare' => '='
+					) )
+				) );
 
-				$posts = get_posts(array(
-					'post_type'		=> 'omise_charge_items',
-					'meta_query' 	=> array(array(
-						'key' 		=> '_wc_order_id',
-						'value' 	=> $order_id,
-						'compare'	=> '='
-					))
-				));
-
-				if (empty($posts))
+				if ( empty( $posts ) )
 					wp_die( "Charge was not found", "Omise Payment Gateway: Checkout", array( 'response' => 500 ) );
 
 				$order = wc_get_order($order_id);
-				if (!$order)
+				if ( ! $order )
 					wp_die( "Order was not found", "Omise Payment Gateway: Checkout", array( 'response' => 500 ) );
 
-				$confirmed_url 	= get_post_custom_values('_wc_confirmed_url', $posts[0]->ID);
-				$confirmed_url 	= $confirmed_url[0];
+				$charge_id     = get_post_custom_values( '_omise_charge_id', $posts[0]->ID );
+				$charge_id     = $charge_id[0];
+				$confirmed_url = get_post_custom_values( '_wc_confirmed_url', $posts[0]->ID );
+				$confirmed_url = $confirmed_url[0];
 
-				$charge_id 		= get_post_custom_values('_omise_charge_id', $posts[0]->ID);
-				$charge_id 		= $charge_id[0];
+				$result = Omise::get_charge( $this->private_key, $charge_id );
 
-				$result = Omise::get_charge($this->private_key, $charge_id);
+				// Auto capture or not?
+				if ( "auto_capture" === $this->payment_action ) {
+					if ( $this->is_charge_paid( $result ) ) {
+						$order->payment_complete();
+						$order->add_order_note( 'Payment with Omise successful' );
 
-				if ($this->is_charge_success($result)) {
-					$order->payment_complete();
-					$order->add_order_note('Payment with Omise successful');
+						// Remove cart
+						WC()->cart->empty_cart();
 
-					// Remove cart
-					WC()->cart->empty_cart();
-
-					header("Location: ".$confirmed_url);
-					die();
-				} else {
-					if ($result->failure_code && $result->failure_message) {
-						$order->add_order_note('Charge was not completed, '.$result->failure_message);
-						wp_die($result->failure_message, "Charge was not completed", array( 'response' => 500 ));
-					} else {
-						wp_die("Charge still in progress", "Charge still in progress", array( 'response' => 500 ));
+						header( "Location: " . $confirmed_url );
+						die();
 					}
+				} else if ( "manual_capture" === $this->payment_action ) {
+					if ( $this->is_charge_authorized( $result ) ) {
+						$order->add_order_note( 'Payment with Omise - authorized' );
+
+						// Remove cart
+						WC()->cart->empty_cart();
+
+						header( "Location: " . $confirmed_url );
+						die();
+					}
+				}
+
+				if ( $result->failure_code && $result->failure_message ) {
+					$order->add_order_note( 'Charge was not completed, ' . $result->failure_message );
+					wp_die( $result->failure_message, "Charge was not completed", array( 'response' => 500 ) );
+				} else {
+					wp_die( "Charge still in progress", "Charge still in progress", array( 'response' => 500 ) );
 				}
 
 				wp_die( "Access denied", "Access Denied", array( 'response' => 401 ) );
@@ -420,7 +501,7 @@ function register_omise_wc_gateway_plugin() {
 	}
 
 	function add_omise_gateway( $methods ) {
-		$methods [] = 'WC_Gateway_Omise';
+		$methods[] = 'WC_Gateway_Omise';
 		return $methods;
 	}
 
