57de4d2274539413355711deea6f9c4179fd4377
Saroj Saengphongumphai <ss.saroj@gmail.com>
2016-06-06 18:28:25 +0700

    Format code and remove value passed to parent's constructor
    For the parameters, singular and plural, passed to parent class, they do
    not affect anything.
    
    So I decided to pass the empty value to prevent the misunderstand.

--- includes/classes/class-omise-transfers-table.php
+++ includes/classes/class-omise-transfers-table.php
@@ -2,66 +2,67 @@
 defined( 'ABSPATH' ) or die( "No direct script access allowed." );
 
 if ( ! class_exists( 'WP_List_Table' ) ) {
-	require_once (ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
 
 if ( ! class_exists( 'Omise_Transfers_Table' ) ) {
 	class Omise_Transfers_Table extends WP_List_Table {
 
 		function __construct( $omise_list_object ) {
-			parent::__construct( array (
-					'singular' => 'omise_transfer',
-					'plural' => 'omise_transfers',
-					'ajax' => false 
+			parent::__construct( array(
+					'singular' => '',
+					'plural' => '',
+					'ajax' => false
 			) );
 			
 			$this->items = $omise_list_object;
 		}
 
 		function get_table_classes() {
-			return array (
+			return array(
 					'widefat',
 					'striped',
-					$this->_args['plural'] 
+					$this->_args['plural']
 			);
 		}
 
 		function prepare_items() {
 			$columns = $this->get_columns();
-			$hidden = array ();
-			$sortable = array ();
+			$hidden = array();
+			$sortable = array();
 			
 			$totalitems = $this->items['total'];
 			$perpage = $this->items['limit'];
 			$paged = isset( $_GET['paged'] ) ? $_GET['paged'] : 1;
 			$totalpages = ceil( $totalitems / $perpage );
 			
-			$this->set_pagination_args( array (
+			$this->set_pagination_args( array(
 					'total_items' => $totalitems,
 					'total_pages' => $totalpages,
-					'per_page' => $perpage 
+					'per_page' => $perpage
 			) );
 			
-			$this->_column_headers = array (
+			$this->_column_headers = array(
 					$columns,
 					$hidden,
-					$sortable 
+					$sortable
 			);
 			
 			// Clear item property to call `no_items` method.
-			if ( $this->items['total'] <= 0 )
-				$this->items = array ();
+			if ( $this->items['total'] <= 0 ) {
+				$this->items = array();
+			}
 		}
 
 		function get_columns() {
-			return $columns = array (
+			return $columns = array(
 					'trsf_amount' => __( 'Amount' ),
 					'trsf_id' => __( 'Transfer Id' ),
 					'trsf_sent' => __( 'Sent' ),
 					'trsf_paid' => __( 'Paid' ),
 					'trsf_failure' => __( 'Failure Message' ),
 					'trsf_datetime' => __( 'Created' ),
-					'trsf_action' => '' 
+					'trsf_action' => ''
 			);
 		}
 
@@ -82,9 +83,17 @@ if ( ! class_exists( 'Omise_Transfers_Table' ) ) {
 		}
 
 		function column_trsf_amount( $record ) {
-			$class = ($record['failure_code']) ? 'TextDanger' : ((! $record['sent'] || ! $record['paid']) ? 'TextWarning' : 'TextSuccess');
+			$class = 'TextSuccess';
 			
-			echo "<strong class='Omise-" . $class . "'>" . OmisePluginHelperCurrency::format( $record['currency'], $record['amount'] ) . "</strong>";
+			if ( $record['failure_code'] ) {
+				$class = 'TextDanger';
+			} else {
+				if ( ! $record['sent'] || ! $record['paid'] ) {
+					$class = 'TextWarning';
+				}
+			}
+			
+			echo "<strong class='Omise-$class'>" . OmisePluginHelperCurrency::format( $record['currency'], $record['amount'] ) . "</strong>";
 		}
 
 		function column_trsf_id( $record ) {
