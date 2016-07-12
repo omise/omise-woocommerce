3f06d57ed635c02a3ad036cfa49fa42951090e1e
Saroj Saengphongumphai <ss.saroj@gmail.com>
2016-07-12 18:04:25 +0700

    Add words that need to translate
    Those words are the words and messages that appears at front-end, card payment form.

--- includes/templates/omise-payment-form.php
+++ includes/templates/omise-payment-form.php
@@ -1,26 +1,27 @@
 <div id="omise_cc_form">
 <?php 
 	$showExistingCards = $viewData["user_logged_in"] && isset($viewData["existingCards"]->data) && sizeof($viewData["existingCards"]->data) > 0;
-	if($showExistingCards){
+	if ( $showExistingCards ) {
 ?>
 <p class="form-row form-row-wide">
-	Select card : <br/>
+	<?php echo Omise_Util::translate( 'Select card' ); ?> : <br/>
+	
 		<?php 
-			foreach($viewData["existingCards"]->data as $card){
-				echo "<input type='radio' name='card_id' value='{$card->id}' />Card ends with {$card->last_digits}<br/>";
+			foreach ( $viewData["existingCards"]->data as $card ) {
+				echo "<input type='radio' name='card_id' value='{$card->id}' />" . Omise_Util::translate( 'Card ends with' ) . "{$card->last_digits}<br/>";
 			}
 		?>
 </p>
-&nbsp;<input type='radio' id='new_card_info' name='card_id' value='' />New payment information
+&nbsp;<input type='radio' id='new_card_info' name='card_id' value='' /><?php echo Omise_Util::translate( 'New payment information' ); ?>
 <?php } ?>
 <fieldset id="new_card_form" class="<?php echo $showExistingCards ? 'omise-hidden':''; ?>">
 	<?php 
 		require_once('omise-cc-form.php'); 
-		if($viewData["user_logged_in"]){
+		if ( $viewData["user_logged_in"] ) {
 	?>
 	<p class="form-row form-row-wide">
 		<input type='checkbox' name='omise_save_customer_card' id='omise_save_customer_card' /> 
-		<label for="omise_save_customer_card" class="inline">Save card for next time</label>
+		<label for="omise_save_customer_card" class="inline"><?php echo Omise_Util::translate( 'Save card for next time' ); ?></label>
 	</p>
 	<?php 
 		}
