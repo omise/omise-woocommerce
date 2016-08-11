405554e03323fe9624b84738bf9907b74d17318d
Saroj Saengphongumphai <ss.saroj@gmail.com>
2016-08-11 16:24:50 +0700

    Add translatable word for a page header, Transfers History
    - Remove a sub-tab, Transfers

--- includes/templates/omise-wp-admin-transfer-page.php
+++ includes/templates/omise-wp-admin-transfer-page.php
@@ -5,11 +5,7 @@
 
 		<?php Omise_Util::render_partial( 'header', $viewData ); ?>
 
-		<h1>Transfers History</h1>
-
-		<h2 class="nav-tab-wrapper wp-clearfix">
-			<a href="<?php echo esc_url( add_query_arg( array( 'page' => 'omise-plugin-admin-transfer-page' ), admin_url( 'admin.php' ) ) ); ?>" class="nav-tab nav-tab-active"><?php esc_html_e( 'Transfers' ); ?></a>
-		</h2>
+		<h1><?php echo Omise_Util::translate( 'Transfers History' ); ?></h1>
 
 		<div id="Omise-TransferList">
 			<form method="get">
