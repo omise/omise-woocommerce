679d41972ae7a9b016cfe87c8cf494af3feaf0a3
guzzilar <iam.nuttanon@gmail.com>
2017-04-20 13:53:03 +0700

    Include Autoloader class before running the test.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@
  * @package Omise
  */
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
