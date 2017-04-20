e223df78a3fc4dd7df291e61e02e184f4417a218
guzzilar <iam.nuttanon@gmail.com>
2017-04-20 13:54:50 +0700

    tests/bootstrap.php :: load omise-woocommerce.php file before start running test.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,7 +19,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/omise.php';
+	require dirname( dirname( __FILE__ ) ) . '/omise-woocommerce.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
