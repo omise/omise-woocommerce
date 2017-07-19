437853378d1376fe98b73f47ae4fd07190acce05
Anak Mirasing <thaihooligan@hotmail.com>
2017-07-19 18:12:03 +0700

    Update message store and message request handler class

--- includes/fbbot/class-omise-fbbot-message-store.php
+++ includes/fbbot/class-omise-fbbot-message-store.php
@@ -11,9 +11,9 @@ if( ! class_exists( 'Omise_Messenger_Bot_Message_Store' ) ) {
 			$user = Omise_Messenger_Bot_User_Service::get_user( $sender_id );
 			$shop_name = get_bloginfo( 'name' );
 
-			$greeting_message_1 = Omise_Util::translate( ':D Hello ' ) . $user->display_name . ' ';
-			$greeting_message_2 = Omise_Util::translate( 'Welcome to ') . $shop_name;
-			$greeting_message_3 = Omise_Util::translate( ', what are you looking for today ?' );
+			$greeting_message_1 = __( ':D Hello ' ) . $user->display_name . ' ';
+			$greeting_message_2 = __( 'Welcome to ') . $shop_name;
+			$greeting_message_3 = __( ', what are you looking for today ?' );
 
 			$greeting_message = $greeting_message_1 . $greeting_message_2 . $greeting_message_3;
 
@@ -21,20 +21,34 @@ if( ! class_exists( 'Omise_Messenger_Bot_Message_Store' ) ) {
 		}
 
 		public static function get_helping_message() {
-			$helping_message = Omise_Util::translate( ":) Don't worry, in spite of the fact i'm just a bot but i can help you. You can choose 1 choice from below." );
+			$helping_message = __( ":) Don't worry, in spite of the fact i'm just a bot but i can help you. You can choose 1 choice from below." );
 
 			return $helping_message;
 		}
 
 		public static function get_unrecognized_message() {
-			$default_message_1 = Omise_Util::translate( ":'(  I wish I could understand you, maybe one day! I’m here to help you shopping on Messenger app, Do you want to buy something ?" );
-			$default_message_2 = Omise_Util::translate( '🤖  Oh, I’m just a bot! but i have a cool stuff for cool people like you. Which do you like best ?' );
+			$default_message_1 = __( ":'(  I wish I could understand you, maybe one day! I’m here to help you shopping on Messenger app, Do you want to buy something ?" );
+			$default_message_2 = __( '🤖  Oh, I’m just a bot! but i have a cool stuff for cool people like you. Which do you like best ?' );
 
 			$default_messages = array( $default_message_1, $default_message_2 );
 
 			$default_message = $default_messages[ mt_rand( 0, count( $default_messages ) - 1 ) ];
 
 			return $default_message;
+		}
+
+		public static function check_greeting_words( $message ) {
+			$greeting_words = array( 'hi', 'hello' );
+			return in_array( $message, $greeting_words );
+		}
+
+		public static function check_helping_words( $message ) {
+			$helping_words = array( 'help' );
+			return in_array( $message, $helping_words );
+		}
+
+		public static function check_order_checking( $message ) {
+			return ( mb_substr( $message, 0, 1 ) == '#' );
 		}
 
 	}
--- includes/fbbot/class-omise-fbbot-request-handler.php
+++ includes/fbbot/class-omise-fbbot-request-handler.php
@@ -12,8 +12,27 @@ class Omise_FBBot_Request_Handler {
 		}
 
 		public static function handle_message_from( $sender_id, $user_message ) {
+			$message_store = Omise_Messenger_Bot_Message_Store;
 			$message = strtolower( $user_message );
       error_log( 'handle message : ' . $message );
+
+      if ( $message_store::check_greeting_words( $message ) ) {
+
+      } else if ( $message_store::check_helping_words( $message ) ){
+
+      } else if ( $message_store::check_order_checking( $message ) ){
+        // Checking order status from order number
+        $checking_text = explode('#', $message);
+
+        if ( ! $checking_text[1] ) {
+          return;
+        }
+
+        $order_id = $checking_text[1];
+
+      } else {
+      	// Handle unrecognize message
+      }
     }
 
     public static function handle_payload_from( $sender_id, $payload ) {
