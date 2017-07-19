fc98e8e534dbdf63f44bd0365b01ffb5028e7391
Anak Mirasing <thaihooligan@hotmail.com>
2017-07-19 11:48:12 +0700

    Update bot endpoint register and configurator

--- includes/fbbot/class-omise-fbbot-configurator.php
+++ includes/fbbot/class-omise-fbbot-configurator.php
@@ -1,14 +1,22 @@
 <?php
 defined( 'ABSPATH' ) or die( "No direct script access allowed." );
 
-if( ! class_exists( 'Omise_Messenger_Bot_Configurator' ) ) {
-	class Omise_Messenger_Bot_Configurator {
-		private static $version = '1';
-		private static $namespace = 'omisemsgbot';
+if ( class_exists( 'Omise_FBBot_Configurator' ) ) {
+  return;
+}
 
-		public static function get_namespace() {
-			return Omise_Messenger_Bot_Configurator::$namespace . '/v' . Omise_Messenger_Bot_Configurator::$version;
-		}
+class Omise_FBBot_Configurator {
+	private static $version = '1';
+	private static $namespace = 'omisemsgbot';
+
+	public static function get_namespace() {
+		return Omise_FBBot_Configurator::$namespace . '/v' . Omise_FBBot_Configurator::$version;
 	}
 
+	public static function get_fb_settings( $id ) {
+		$option = get_option( 'woocommerce_omise_settings', null );
+		return $option[$id];
+	}
 }
+
+
--- omise-woocommerce.php
+++ omise-woocommerce.php
@@ -57,7 +57,7 @@ class Omise {
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/libraries/omise-plugin/Omise.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/class-omise-wc-myaccount.php';
 
-		// Messenger Bot
+		// Facebook Bot
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/fbbot/class-omise-fbbot-endpoints.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/fbbot/class-omise-fbbot-user-service.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/fbbot/class-omise-fbbot-payment-handler.php';
@@ -83,6 +83,9 @@ class Omise {
 		add_action( 'plugins_loaded', 'register_omise_internetbanking', 0 );
 		add_action( 'plugins_loaded', 'prepare_omise_myaccount_panel', 0 );
 		add_action( 'plugins_loaded', array( $this, 'register_user_agent' ), 0 );
+
+		// Facebook Bot Action & Filter
+		add_action( 'rest_api_init', array( Omise_FBBot_Endpoints::get_instance(), 'register_bot_api_routes' ) );
 
 		$this->init_admin();
 	}
