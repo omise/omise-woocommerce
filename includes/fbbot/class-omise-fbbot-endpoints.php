bc57c0d6e9a7b4572e7863b68084890ed340e50b
Anak Mirasing <thaihooligan@hotmail.com>
2017-07-25 15:39:32 +0700

    Use http service to send confirm order message to user

--- includes/fbbot/class-omise-fbbot-endpoints.php
+++ includes/fbbot/class-omise-fbbot-endpoints.php
@@ -154,7 +154,7 @@ class Omise_FBBot_Endpoints extends WP_REST_Controller {
 
         // Update order status here!
         $order_id = $metadata->order_id;
-        Omise_Messenger_Bot_WooCommerce::update_order_status( $order_id, $charge );
+        Omise_FBBot_WooCommerce::update_order_status( $order_id, $charge );
 
         $sender_id = $charge->metadata->messenger_id;
         if( ! isset( $sender_id ) )
@@ -162,7 +162,7 @@ class Omise_FBBot_Endpoints extends WP_REST_Controller {
 
         $thanks_message = Omise_FBBot_Conversation_Generator::thanks_for_purchase_message( $order_id );
         
-        $response = Omise_FBBot_Conversation_Handler::send_message_to( $sender_id, $thanks_message );
+        $response = Omise_FBBot_HTTPService::send_message_to( $sender_id, $thanks_message );
         break;
 
       case 'charge.complete':
@@ -184,7 +184,7 @@ class Omise_FBBot_Endpoints extends WP_REST_Controller {
 
         $thanks_message = Omise_FBBot_Conversation_Generator::thanks_for_purchase_message( $order_id );
         
-        $response = Omise_FBBot_Conversation_Handler::send_message_to( $sender_id, $thanks_message );
+        $response = Omise_FBBot_HTTPService::send_message_to( $sender_id, $thanks_message );
         break;
       
       default:
