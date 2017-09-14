05219399c2e8f1bea16c37b22f376486cf364ddd
Anak Mirasing <thaihooligan@hotmail.com>
2017-09-14 01:25:56 -0400

    Separate fbbot entity to new class and update confidence rate to 0.85 (#63)

---
+++ includes/fbbot/class-omise-fbbot-entity.php
@@ -0,0 +1,15 @@
+<?php
+defined( 'ABSPATH' ) or die( "No direct script access allowed." );
+
+if ( class_exists( 'Omise_FBBot_Entity' ) ) {
+	return;
+}
+
+abstract class Omise_FBBot_Entity {
+	const CONFIDENCE_RATE = 0.85;
+
+	const GREETINGS = "greetings";
+	const USER_GREETINGS = "user_greetings";
+	const CHECK_ORDER_STATUS = "check_order_status";
+	const NEED_HELP = "need_help";
+}
