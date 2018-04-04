69fd8aacdbc7818bfd2f4a50a47559fefe04227f
guzzilar <iam.nuttanon@gmail.com>
2018-04-04 15:10:35 +0700

    Remove legacy files (we no longer use).

--- includes/libraries/omise-plugin/Omise.php
+++ includes/libraries/omise-plugin/Omise.php
@@ -1,5 +1,3 @@
 <?php
 
 require_once dirname(__FILE__).'/helpers/charge.php';
-require_once dirname(__FILE__).'/helpers/currency.php';
-require_once dirname(__FILE__).'/helpers/transaction.php';
--- includes/libraries/omise-plugin/helpers/charge.php
+++ includes/libraries/omise-plugin/helpers/charge.php
@@ -3,29 +3,6 @@ if (! class_exists('OmisePluginHelperCharge')) {
     class OmisePluginHelperCharge
     {
         /**
-         * @param string  $currency
-         * @param integer $amount
-         * @return string
-         */
-        public static function amount($currency, $amount)
-        {
-            switch (strtoupper($currency)) {
-                case 'THB':
-                    // Convert to satang unit
-                    $amount = $amount * 100;
-                    break;
-
-                case 'JPY':
-                    break;
-
-                default:
-                    break;
-            }
-
-            return $amount;
-        }
-
-        /**
          * @param \omise-php\OmiseCharge $charge
          * @return boolean
          */
