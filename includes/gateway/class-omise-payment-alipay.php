e322b3b065e4fb62cb2dc9fa4d70ec520fcdb180
Thee Sritabtim <thammada@gmail.com>
2019-01-23 15:13:04 +0700

    rename hooks and expose metadata.order_id
    - rename hooks to charge_params_description and charge_params_metadata
    - include metadata.order_id for charge_params_metadata filter

--- includes/gateway/class-omise-payment-alipay.php
+++ includes/gateway/class-omise-payment-alipay.php
@@ -88,17 +88,20 @@ function register_omise_alipay() {
 			$order->add_order_note( __( 'Omise: Processing a payment with Alipay solution..', 'omise' ) );
 
 			try {
-				$metadata = apply_filters( 'omise_charge_metadata', array(), $order );
 				$charge = $this->sale( array(
 					'amount'      => $this->format_amount_subunit( $order->get_total(), $order->get_order_currency() ),
 					'currency'    => $order->get_order_currency(),
-					'description' => apply_filters( 'omise_charge_description', 'WooCommerce Order id ' . $order_id, $order ),
+					'description' => apply_filters( 'omise_charge_params_description', 'WooCommerce Order id ' . $order_id, $order ),
 					'offsite'     => 'alipay',
 					'return_uri'  => add_query_arg( 'order_id', $order_id, site_url() . "?wc-api=omise_alipay_callback" ),
-					'metadata'    => array_merge( $metadata, array(
-						/** backward compatible with WooCommerce v2.x series **/
-						'order_id' => version_compare( WC()->version, '3.0.0', '>=' ) ? $order->get_id() : $order->id
-					) )
+					'metadata'    => apply_filters(
+						'omise_charge_params_metadata', 
+						array(
+							/** backward compatible with WooCommerce v2.x series **/
+							'order_id' => version_compare( WC()->version, '3.0.0', '>=' ) ? $order->get_id() : $order->id
+						),
+						$order
+					)
 				) );
 
 				$order->add_order_note( sprintf( __( 'Omise: Charge (ID: %s) has been created', 'omise' ), $charge['id'] ) );
