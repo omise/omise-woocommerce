ed80d2ae481626f9fa50a8bc454ac7d79cd72c5f
guzzilar <iam.nuttanon@gmail.com>
2019-09-23 07:02:51 +0700

    TrueMoney:  Be able to create a new charge.

--- includes/gateway/class-omise-payment-truemoney.php
+++ includes/gateway/class-omise-payment-truemoney.php
@@ -64,12 +64,49 @@ function register_omise_truemoney() {
 		 * @inheritdoc
 		 */
 		public function charge( $order_id, $order ) {
+			$total      = $order->get_total();
+			$currency   = $order->get_order_currency();
+			$return_uri = add_query_arg(
+				array( 'wc-api' => 'omise_truemoney_callback', 'order_id' => $order_id ), home_url()
+			);
+			$metadata   = array_merge(
+				apply_filters( 'omise_charge_params_metadata', array(), $order ),
+				array( 'order_id' => $order_id ) // override order_id as a reference for webhook handlers.
+			);
+
+			return OmiseCharge::create( array(
+				'amount'      => Omise_Money::to_subunit( $total, $currency ),
+				'currency'    => $currency,
+				'description' => apply_filters( 'omise_charge_params_description', 'WooCommerce Order id ' . $order_id, $order ),
+				'source'      => array( 'type' => 'truemoney', 'phone_number' => '' ),
+				'return_uri'  => $return_uri,
+				'metadata'    => $metadata
+			) );
 		}
 
 		/**
 		 * @inheritdoc
 		 */
 		public function result( $order_id, $order, $charge ) {
+			if ( self::STATUS_FAILED == $charge['status'] ) {
+				return $this->payment_failed( $charge['failure_message'] . ' (code: ' . $charge['failure_code'] . ')' );
+			}
+
+			if ( self::STATUS_PENDING == $charge['status'] ) {
+				$order->add_order_note( sprintf( __( 'Omise: Redirecting buyer to %s', 'omise' ), esc_url( $charge['authorize_uri'] ) ) );
+
+				return array (
+					'result'   => 'success',
+					'redirect' => $charge['authorize_uri'],
+				);
+			}
+
+			return $this->payment_failed(
+				sprintf(
+					__( 'Please feel free to try submitting your order again, or contact our support team if you have any questions (Your temporary order id is \'%s\')', 'omise' ),
+					$order_id
+				)
+			);
 		}
 	}
 
