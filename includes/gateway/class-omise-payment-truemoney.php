071d86172d974758192139cfbcf3734aad6dac95
guzzilar <iam.nuttanon@gmail.com>
2019-09-24 07:01:50 +0700

    TrueMoney: Implementing callback function.

--- includes/gateway/class-omise-payment-truemoney.php
+++ includes/gateway/class-omise-payment-truemoney.php
@@ -30,6 +30,7 @@ function register_omise_truemoney() {
 			$this->description = $this->get_option( 'description' );
 
 			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+			add_action( 'woocommerce_api_' . $this->id . '_callback', array( $this, 'callback' ) );
 		}
 
 		/**
@@ -115,6 +116,99 @@ function register_omise_truemoney() {
 					$order_id
 				)
 			);
+		}
+
+		/**
+		 * @return void
+		 */
+		public function callback() {
+			if ( ! isset( $_GET['order_id'] ) || ! $order = $this->load_order( $_GET['order_id'] ) ) {
+				wc_add_notice(
+					wp_kses(
+						__( 'We cannot validate your payment result:<br/>Note that your payment might already has been processed. Please contact our support team if you have any questions.', 'omise' ),
+						array( 'br' => array() )
+					),
+					'error'
+				);
+
+				wp_redirect( wc_get_checkout_url() );
+				exit;
+			}
+
+			$order->add_order_note( __( 'Omise: Validating the payment result..', 'omise' ) );
+
+			try {
+				$charge = OmiseCharge::retrieve( $order->get_transaction_id() );
+
+				if ( self::STATUS_FAILED == $charge['status'] ) {
+					throw new Exception( $charge['failure_message'] . ' (code: ' . $charge['failure_code'] . ')' );
+				}
+
+				if ( self::STATUS_SUCCESSFUL == $charge['status'] && $charge['paid'] ) {
+					$order->add_order_note(
+						sprintf(
+							wp_kses(
+								__( 'Omise: Payment successful.<br/>An amount of %1$s %2$s has been paid', 'omise' ),
+								array( 'br' => array() )
+							),
+							$order->get_total(),
+							$order->get_order_currency()
+						)
+					);
+
+					$order->payment_complete();
+
+					WC()->cart->empty_cart();
+
+					wp_redirect( $order->get_checkout_order_received_url() );
+					exit;
+				}
+
+				if ( self::STATUS_PENDING == $charge['status'] && ! $charge['paid'] ) {
+					$order->add_order_note(
+						wp_kses(
+							__( 'Omise: The payment has been processing.<br/>Due to the TrueMoney provider, this might takes a few seconds or an hour. Please do a manual \'Sync Payment Status\' action from the Order Actions panel or check the payment status directly at Omise dashboard again later', 'omise' ),
+							array( 'br' => array() )
+						)
+					);
+
+					$order->update_status( 'on-hold' );
+
+					wp_redirect( $order->get_checkout_order_received_url() );
+					exit;
+				}
+
+				throw new Exception( __( 'Note that your payment may have already been processed. Please contact our support team if you have any questions.', 'omise' ) );
+			} catch ( Exception $e ) {
+				wc_add_notice(
+					sprintf(
+						wp_kses(
+							__( 'It seems we\'ve been unable to process your payment properly:<br/>%s', 'omise' ),
+							array( 'br' => array() )
+						),
+						$e->getMessage()
+					),
+					'error'
+				);
+
+				$order->add_order_note(
+					sprintf(
+						wp_kses(
+							__( 'Omise: Payment failed.<br/>%s', 'omise' ),
+							array( 'br' => array() )
+						),
+						$e->getMessage()
+					)
+				);
+
+				$order->update_status( 'failed' );
+
+				wp_redirect( wc_get_checkout_url() );
+				exit;
+			}
+
+			wp_die( 'Access denied', 'Access Denied', array( 'response' => 401 ) );
+			die();
 		}
 	}
 
