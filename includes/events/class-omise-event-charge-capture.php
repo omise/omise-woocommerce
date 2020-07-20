7598b4bdd76920fbad576c5fffbf0ed5587d3520
guzzilar <iam.nuttanon@gmail.com>
2020-07-20 11:56:38 +0700

    Event Handler, charge.capture: bringing back the 'resolve' method as it is being used to handle a capture action from Omise Dashboard.

--- includes/events/class-omise-event-charge-capture.php
+++ includes/events/class-omise-event-charge-capture.php
@@ -36,4 +36,49 @@ class Omise_Event_Charge_Capture extends Omise_Event {
 
 		return true;
 	}
+
+	/**
+	 * This `charge.capture` event is only being used
+	 * to catch a manual-capture action that happens on 'Omise Dashboard'.
+	 * For on-store capture, it will be handled by Omise_Payment_Creditcard::process_capture.
+	 */
+	public function resolve() {
+		$this->order->add_order_note( __( 'Omise: Received charge.capture webhook event.', 'omise' ) );
+
+		switch ( $this->data['status'] ) {
+			case 'failed':
+				if ( $this->order->has_status( 'failed' ) ) {
+					return;
+				}
+
+				$message         = __( 'Omise: Payment failed.<br/>%s', 'omise' );
+				$failure_message = $this->data['failure_message'] . ' (code: ' . $this->data['failure_code'] . ')';
+				$this->order->add_order_note(
+					sprintf(
+						wp_kses( $message, array( 'br' => array() ) ),
+						$failure_message
+					)
+				);
+				$this->order->update_status( 'failed' );
+				break;
+
+			case 'successful':
+				$message = __( 'Omise: Payment successful.<br/>An amount %1$s %2$s has been paid', 'omise' );
+
+				$this->order->add_order_note(
+					sprintf(
+						wp_kses( $message, array( 'br' => array() ) ),
+						$this->order->get_total(),
+						$this->order->get_currency()
+					)
+				);
+
+				if ( ! $this->order->has_status( 'processing' ) ) {
+					$this->order->update_status( 'processing' );
+				}
+			break;
+		}
+
+		return;
+	}
 }
