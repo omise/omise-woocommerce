8027ab1735dab5064caa1e4eec97a7375ffbbf83
mayurkathale <mayur.kathale@gmail.com>
2020-11-30 16:01:41 +0700

    Improving text on order status page
    Co-authored-by: Daniel Fowler <daniel@omise.co>

--- includes/gateway/class-omise-payment-paynow.php
+++ includes/gateway/class-omise-payment-paynow.php
@@ -102,7 +102,7 @@ class Omise_Payment_Paynow extends Omise_Payment_Offline {
 						<?php echo __( 'We\'ve received your payment.', 'omise' ); ?>
 					</div>
 					<div class="timeout" style="display:none">
-						<?php echo __( 'Payment session timed out. You can still complete QR payment by scanning code sent on your email.', 'omise' ); ?>
+						<?php echo __( 'Payment session timed out. You can still complete QR payment by scanning the code sent to your email address.', 'omise' ); ?>
 					</div>
 				</div>
 			</div>
