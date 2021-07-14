16bead127d67d46d893e5b6d292db554ef2655bb
Donut <ponpol@omise.co>
2021-07-14 12:27:50 +0700

    indentation

--- includes/gateway/class-omise-payment-fpx.php
+++ includes/gateway/class-omise-payment-fpx.php
@@ -19,7 +19,7 @@ class Omise_Payment_FPX extends Omise_Payment_Offsite {
 		$this->title                = $this->get_option( 'title' );
 		$this->description          = $this->get_option( 'description' );
 		$this->restricted_countries = array( 'MY' );
-    $this->backend     					= new Omise_Backend_FPX;
+		$this->backend     					= new Omise_Backend_FPX;
 
 		add_action( 'woocommerce_api_' . $this->id . '_callback', 'Omise_Callback::execute' );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
