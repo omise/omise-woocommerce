2a51596c09f8ac2e4a402021d0a3564a00c62152
Pittayarat Saengow <pittayarat@omise.co>
2021-12-02 14:45:40 +0700

    Adjust mobile banking item style

--- templates/payment/form-mobilebanking.php
+++ templates/payment/form-mobilebanking.php
@@ -2,14 +2,16 @@
 	<fieldset id="omise-form-mobliebanking">
 		<ul class="omise-banks-list">
 			<?php foreach ( $viewData['mobile_banking_backends'] as $backend ) : ?>
-				<li class="item">
-					<input id="<?php echo $backend->_id; ?>" type="radio" name="omise-offsite" value="<?php echo $backend->_id; ?>" />
-					<label for="<?php echo $backend->_id; ?>">
-						<div class="mobile-banking-logo <?php echo $backend->provider_logo; ?>"></div>
-						<div class="bank-label">
-							<span class="title"><?php echo $backend->provider_name; ?></span><br/>
-						</div>
-					</label>
+				<li class="item mobile-banking">
+					<div>
+						<input id="<?php echo $backend->_id; ?>" type="radio" name="omise-offsite" value="<?php echo $backend->_id; ?>" />
+						<label for="<?php echo $backend->_id; ?>">
+							<div class="mobile-banking-logo <?php echo $backend->provider_logo; ?>"></div>
+							<div class="mobile-banking-label">
+								<span class="title"><?php echo $backend->provider_name; ?></span><br/>
+							</div>
+						</label>
+					</div>	
 				</li>
 			<?php endforeach; ?>
 		</ul>
