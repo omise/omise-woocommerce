cab46f49b73c1f16053f984e97c7d0f73f553b49
Kan <kan@omise.co>
2022-04-04 14:46:59 +0700

    commit

--- omise-woocommerce.php
+++ omise-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Omise Payment Gateway
  * Plugin URI:  https://www.omise.co/woocommerce
  * Description: Omise WooCommerce Gateway Plugin is a WordPress plugin designed specifically for WooCommerce. The plugin adds support for Omise Payment Gateway payment method to WooCommerce.
- * Version:     4.18
+ * Version:     4.19
  * Author:      Omise and contributors
  * Author URI:  https://github.com/omise/omise-woocommerce/graphs/contributors
  * Text Domain: omise
@@ -20,7 +20,7 @@ class Omise {
 	 *
 	 * @var string
 	 */
-	public $version = '4.18';
+	public $version = '4.19';
 
 	/**
 	 * The Omise Instance.
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Omise
 Tags: omise, payment, payment gateway, woocommerce plugin, installment, internet banking, alipay, paynow, truemoney wallet, woocommerce payment
 Requires at least: 4.3.1
 Tested up to: 5.9.0
-Stable tag: 4.18
+Stable tag: 4.19
 License: MIT
 License URI: https://opensource.org/licenses/MIT
 
