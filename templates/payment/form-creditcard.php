661e1a60f2a15d4108c8b0c528230208bcdfa17e
radiant7766 <95275075+radiant7766@users.noreply.github.com>
2022-05-07 10:25:15 +0700

    Identify card type from card number
    I am trying to identify card number to display the credit card processing company icon on the side of the field. So far I can identify the card number now but I can't display the icon on the side. It only returns the company name which does not work. Can you help me out?

--- templates/payment/form-creditcard.php
+++ templates/payment/form-creditcard.php
@@ -51,3 +51,55 @@
 		this.value = format_cardnumber(this.value)
 	}
 </script>
+
+<script type="text/javascript">
+
+  function GetCardType(number)
+  {
+      // visa
+      var re = new RegExp("^4");
+      if (number.match(re) != null)
+          return "Visa";
+
+      // Mastercard 
+      // Updated for Mastercard 2017 BINs expansion
+       if (/^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/.test(number)) 
+          return "Mastercard";
+
+      // AMEX
+      re = new RegExp("^3[47]");
+      if (number.match(re) != null)
+          return "AMEX";
+
+      // Discover
+      re = new RegExp("^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)");
+      if (number.match(re) != null)
+          return "Discover";
+
+      // Diners
+      re = new RegExp("^36");
+      if (number.match(re) != null)
+          return "Diners";
+
+      // Diners - Carte Blanche
+      re = new RegExp("^30[0-5]");
+      if (number.match(re) != null)
+          return "Diners - Carte Blanche";
+
+      // JCB
+      re = new RegExp("^35(2[89]|[3-8][0-9])");
+      if (number.match(re) != null)
+          return "JCB";
+
+      // Visa Electron
+      re = new RegExp("^(4026|417500|4508|4844|491(3|7))");
+      if (number.match(re) != null)
+          return "Visa Electron";
+
+      return "";
+  }
+  
+  document.getElementById('omise_card_number').oninput = function() {
+    this.value = GetCardType(this.value)
+  }
+</script>
