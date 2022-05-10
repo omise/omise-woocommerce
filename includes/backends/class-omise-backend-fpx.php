b26759400a8e535ce6fbe9b1825caf296025efe5
Aashish <aashish@omise.co>
2022-05-10 16:02:27 +0700

    [FRON-1463]: The method 'disable_merchant_order_on_hold' is made into a static method because it is passed as an static method to the WP filter.

--- includes/backends/class-omise-backend-fpx.php
+++ includes/backends/class-omise-backend-fpx.php
@@ -21,7 +21,12 @@ class Omise_Backend_FPX extends Omise_Backend {
 		$providers = $this->capabilities()->getFPXBanks();
 		$first_value = reset($providers);
 
-		if (property_exists($first_value, 'banks')) {
+		// Preventing the following error:
+		// Uncaught TypeError: property_exists(): Argument #1 must be of type object|string, bool given
+		$typeofFirstValue = gettype($first_value);
+		$isObjectOrString = 'object' === $typeofFirstValue || 'string' === $typeofFirstValue;
+
+		if ($isObjectOrString && property_exists($first_value, 'banks')) {
 			return $first_value->banks;
 		}
 	}
