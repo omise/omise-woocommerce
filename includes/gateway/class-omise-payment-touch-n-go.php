931cf3ce332ffcc46554aa5cd82c34d0416bbd4d
Pittayarat Saengow <pittayarat@omise.co>
2022-07-18 17:35:07 +0700

    update touch n go

--- includes/gateway/class-omise-payment-touch-n-go.php
+++ includes/gateway/class-omise-payment-touch-n-go.php
@@ -5,13 +5,14 @@ class Omise_Payment_TouchNGo extends Omise_Payment_Offsite {
 	public function __construct() {
 		parent::__construct();
 
+		$this->backend     = new Omise_Backend_TouchNGo;
+
 		$this->id                 = 'omise_touch_n_go';
 		$this->has_fields         = false;
-		$this->method_title       = __( 'Omise Touch \'n Go', 'omise' );
-		$this->method_description = __( 'Accept payment through <strong>Touch \'n Go</strong> via Omise payment gateway.', 'omise' );
+		$this->method_title       = __( 'Omise ' . $this->GetMethodTitle(), 'omise' );
+		$this->method_description = __( 'Accept payment through <strong>' . $this->GetMethodTitle() . '</strong> via Omise payment gateway.', 'omise' );
 		$this->supports           = array( 'products', 'refunds' );
 
-		$this->backend     = new Omise_Backend_TouchNGo;
 		$this->init_form_fields();
 		$this->init_settings();
 
@@ -19,7 +20,6 @@ class Omise_Payment_TouchNGo extends Omise_Payment_Offsite {
 		$this->description          = $this->get_option( 'description' );
 		$this->restricted_countries = array( 'SG', 'MY' );
 		
-
 		add_action( 'woocommerce_api_' . $this->id . '_callback', 'Omise_Callback::execute' );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 		add_action( 'woocommerce_order_action_' . $this->id . '_sync_payment', array( $this, 'sync_payment' ) );
@@ -30,54 +30,45 @@ class Omise_Payment_TouchNGo extends Omise_Payment_Offsite {
 	 * @see woocommerce/includes/abstracts/abstract-wc-settings-api.php
 	 */
 	public function init_form_fields() {
+		$method_title = 'Touch \'n Go';
+		$default_title = 'Touch \'n Go';
+
 		if ($this->backend->get_provider() === 'Alipay_plus') {
-			$this->form_fields = array(
-				'enabled' => array(
-					'title'   => __( 'Enable/Disable', 'omise' ),
-					'type'    => 'checkbox',
-					'label'   => __( 'Enable Omise TNG eWallet Payment', 'omise' ),
-					'default' => 'no'
-				),
-	
-				'title' => array(
-					'title'       => __( 'Title', 'omise' ),
-					'type'        => 'text',
-					'description' => __( 'This controls the title the user sees during checkout.', 'omise' ),
-					'default'     => __( 'TNG eWallet (Alipay+™ Partner)', 'omise' ),
-				),
-	
-				'description' => array(
-					'title'       => __( 'Description', 'omise' ),
-					'type'        => 'textarea',
-					'description' => __( 'This controls the description the user sees during checkout.', 'omise' )
-				),
-			);
+			$method_title = 'TNG eWallet';
+			$default_title = 'TNG eWallet (Alipay+™ Partner)';
 		}
-		else {
-			$this->form_fields = array(
-				'enabled' => array(
-					'title'   => __( 'Enable/Disable', 'omise' ),
-					'type'    => 'checkbox',
-					'label'   => __( 'Enable Omise Touch \'n Go Payment', 'omise' ),
-					'default' => 'no'
-				),
-	
-				'title' => array(
-					'title'       => __( 'Title', 'omise' ),
-					'type'        => 'text',
-					'description' => __( 'This controls the title the user sees during checkout.', 'omise' ),
-					'default'     => __( 'Touch \'n Go', 'omise' ),
-				),
-	
-				'description' => array(
-					'title'       => __( 'Description', 'omise' ),
-					'type'        => 'textarea',
-					'description' => __( 'This controls the description the user sees during checkout.', 'omise' )
-				),
-			);
-		}
+
+		$this->form_fields = array(
+			'enabled' => array(
+				'title'   => __( 'Enable/Disable', 'omise' ),
+				'type'    => 'checkbox',
+				'label'   => __( 'Enable Omise ' . $method_title . ' Payment', 'omise' ),
+				'default' => 'no'
+			),
+
+			'title' => array(
+				'title'       => __( 'Title', 'omise' ),
+				'type'        => 'text',
+				'description' => __( 'This controls the title the user sees during checkout.', 'omise' ),
+				'default'     => __( $default_title, 'omise' ),
+			),
+
+			'description' => array(
+				'title'       => __( 'Description', 'omise' ),
+				'type'        => 'textarea',
+				'description' => __( 'This controls the description the user sees during checkout.', 'omise' )
+			),
+		);
 	}
 		
+	public function GetMethodTitle() {
+		if ($this->backend->get_provider() === 'Alipay_plus') {
+			return 'TNG eWallet';
+		}
+		else {
+			return 'Touch \'n Go';
+		}
+	}
 
 	/**
 	 * @inheritdoc
