ad27d9dd743e43d4834be8f462c7938ddaf0297b
Aashish <aashish@omise.co>
2022-09-08 10:42:13 +0545

    [ENGA3-438]: Added missing helper file.

--- includes/libraries/omise-plugin/Omise.php
+++ includes/libraries/omise-plugin/Omise.php
@@ -4,3 +4,4 @@ require_once dirname(__FILE__).'/helpers/charge.php';
 require_once dirname(__FILE__).'/helpers/wc_order.php';
 require_once dirname(__FILE__).'/helpers/mailer.php';
 require_once dirname(__FILE__).'/helpers/request.php';
+require_once dirname(__FILE__).'/helpers/token.php';
