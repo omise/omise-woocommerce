7c8a4d859af18f7d832b7e1fccf24069894fa62f
Zin Kyaw Kyaw <zin@opn.ooo>
2022-09-30 17:09:46 +0700

    [ENGA3-430] : Fixed sonarcloud issue

--- includes/gateway/class-omise-payment-fpx.php
+++ includes/gateway/class-omise-payment-fpx.php
@@ -1,38 +1,41 @@
 <?php
-defined( 'ABSPATH' ) or die( 'No direct script access allowed.' );
+defined('ABSPATH') or die('No direct script access allowed.');
 
-class Omise_Payment_FPX extends Omise_Payment_Offsite {
+class Omise_Payment_FPX extends Omise_Payment_Offsite
+{
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct();
 
 		$this->id                 = 'omise_fpx';
 		$this->has_fields         = true;
-		$this->method_title       = __( 'Omise FPX', 'omise' );
-		$this->method_description = __( 'Accept payment through FPX', 'omise' );
-		$this->supports           = array( 'products', 'refunds' );
+		$this->method_title       = __('Omise FPX', 'omise');
+		$this->method_description = __('Accept payment through FPX', 'omise');
+		$this->supports           = array('products', 'refunds');
 
 		$this->init_form_fields();
 		$this->init_settings();
 
-		$this->title                = $this->get_option( 'title' );
-		$this->description          = $this->get_option( 'description' );
-		$this->restricted_countries = array( 'MY' );
+		$this->title                = $this->get_option('title');
+		$this->description          = $this->get_option('description');
+		$this->restricted_countries = array('MY');
 		$this->source_type          = 'fpx';
 		$this->backend              = new Omise_Backend_FPX;
-		add_action( 'woocommerce_after_checkout_validation', array( $this, 'check_bank_selected' ), 9999, 2 );
-		add_action( 'woocommerce_api_' . $this->id . '_callback', 'Omise_Callback::execute' );
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-		add_action( 'woocommerce_order_action_' . $this->id . '_sync_payment', array( $this, 'sync_payment' ) );
+		add_action('woocommerce_after_checkout_validation', array($this, 'check_bank_selected'), 9999, 2);
+		add_action('woocommerce_api_' . $this->id . '_callback', 'Omise_Callback::execute');
+		add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+		add_action('woocommerce_order_action_' . $this->id . '_sync_payment', array($this, 'sync_payment'));
 	}
 
 	/**
 	 * Add validation to select FPX bank
 	 */
-	public function check_bank_selected( $fields, $errors ) {
-		$source_bank = isset( $fields['source']['bank'] ) ? $fields['source']['bank'] : '';
-		if(empty($source_bank)) {
-			$errors->add( 'validation', __( 'Please select FPX bank below', 'omise' ) );
+	public function check_bank_selected($fields, $errors)
+	{
+		$source_bank = isset($_POST['source']['bank']) ? $_POST['source']['bank'] : '';
+		if (empty($source_bank)) {
+			$errors->add('validation', __('Please select FPX bank below', 'omise'));
 		}
 	}
 
@@ -40,34 +43,36 @@ class Omise_Payment_FPX extends Omise_Payment_Offsite {
 	 * @see WC_Settings_API::init_form_fields()
 	 * @see woocommerce/includes/abstracts/abstract-wc-settings-api.php
 	 */
-	public function init_form_fields() {
+	public function init_form_fields()
+	{
 		$this->form_fields = array(
 			'enabled' => array(
-				'title'   => __( 'Enable/Disable', 'omise' ),
+				'title'   => __('Enable/Disable', 'omise'),
 				'type'    => 'checkbox',
-				'label'   => __( 'Enable Omise FPX Payment', 'omise' ),
+				'label'   => __('Enable Omise FPX Payment', 'omise'),
 				'default' => 'no'
 			),
 
 			'title' => array(
-				'title'       => __( 'Title', 'omise' ),
+				'title'       => __('Title', 'omise'),
 				'type'        => 'text',
-				'description' => __( 'This controls the title the user sees during checkout.', 'omise' ),
-				'default'     => __( 'Online Banking (FPX)', 'omise' ),
+				'description' => __('This controls the title the user sees during checkout.', 'omise'),
+				'default'     => __('Online Banking (FPX)', 'omise'),
 			),
 
 			'description' => array(
-				'title'       => __( 'Description', 'omise' ),
+				'title'       => __('Description', 'omise'),
 				'type'        => 'textarea',
-				'description' => __( 'This controls the description the user sees during checkout.', 'omise' )
+				'description' => __('This controls the description the user sees during checkout.', 'omise')
 			),
 		);
-  }
-  
-    /**
+	}
+
+	/**
 	 * @inheritdoc
 	 */
-	public function payment_fields() {
+	public function payment_fields()
+	{
 		parent::payment_fields();
 		$currency   = get_woocommerce_currency();
 		$cart_total = WC()->cart->total;
@@ -83,12 +88,13 @@ class Omise_Payment_FPX extends Omise_Payment_Offsite {
 	/**
 	 * @inheritdoc
 	 */
-	public function charge( $order_id, $order ) {
-		$source_bank	= isset( $_POST['source']['bank'] ) ? $_POST['source']['bank'] : '';
+	public function charge($order_id, $order)
+	{
+		$source_bank	= isset($_POST['source']['bank']) ? $_POST['source']['bank'] : '';
 
 		$metadata = array_merge(
-			apply_filters( 'omise_charge_params_metadata', array(), $order ),
-			array( 'order_id' => $order_id ) // override order_id as a reference for webhook handlers.
+			apply_filters('omise_charge_params_metadata', array(), $order),
+			array('order_id' => $order_id) // override order_id as a reference for webhook handlers.
 		);
 		$return_uri = add_query_arg(
 			array(
@@ -98,18 +104,18 @@ class Omise_Payment_FPX extends Omise_Payment_Offsite {
 			home_url()
 		);
 
-		return OmiseCharge::create( array(
-			'amount'      => Omise_Money::to_subunit( $order->get_total(), $order->get_currency() ),
+		return OmiseCharge::create(array(
+			'amount'      => Omise_Money::to_subunit($order->get_total(), $order->get_currency()),
 			'currency'    => $order->get_currency(),
-			'description' => apply_filters( 'omise_charge_params_description', 'WooCommerce Order id ' . $order_id, $order ),
-			'source'      => array( 'type' => $this->source_type ),
+			'description' => apply_filters('omise_charge_params_description', 'WooCommerce Order id ' . $order_id, $order),
+			'source'      => array('type' => $this->source_type),
 			'source'      => array(
 				'type'      => 'fpx',
-				'bank' => sanitize_text_field( $source_bank ),
+				'bank' => sanitize_text_field($source_bank),
 			),
 			'return_uri'  => $return_uri,
 			'metadata'    => $metadata
-		) );
+		));
 	}
 
 	/**
@@ -117,14 +123,14 @@ class Omise_Payment_FPX extends Omise_Payment_Offsite {
 	 *
 	 * @see WC_Payment_Gateway::get_icon()
 	 */
-	public function get_icon() {
-		$icon = Omise_Image::get_image( array(
-			    'file' => 'fpx.svg',
-			    'alternate_text' => 'FPX',
-			    'width' => 60,
-			    'height' => 60,
+	public function get_icon()
+	{
+		$icon = Omise_Image::get_image(array(
+			'file' => 'fpx.svg',
+			'alternate_text' => 'FPX',
+			'width' => 60,
+			'height' => 60,
 		));
-		return apply_filters( 'woocommerce_gateway_icon', $icon, $this->id );
+		return apply_filters('woocommerce_gateway_icon', $icon, $this->id);
 	}
-
 }
