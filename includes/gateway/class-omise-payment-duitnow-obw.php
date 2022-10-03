992329e88780c12013d295e3d1ab5033a31b9f44
AJ <108650842+ajzkk@users.noreply.github.com>
2022-10-03 12:28:39 +0700

    [ENGA3-430] : Validation on FPX checkout to select bank (#316)
    * [ENGA3-430] : added validation to select fpx bank
    
    * [ENGA3-430] added jp translation to select fpx bank
    
    * [ENGA3-430] : added visibility of function check_bank_selected
    
    * [ENGA3-430] : Fixed sonarcloud issue
    
    * [ENGA3-430] : added validation for duitnow_obw to select bank

--- includes/gateway/class-omise-payment-duitnow-obw.php
+++ includes/gateway/class-omise-payment-duitnow-obw.php
@@ -1,61 +1,66 @@
 <?php
-defined( 'ABSPATH' ) or die( 'No direct script access allowed.' );
+defined('ABSPATH') or die('No direct script access allowed.');
 
-class Omise_Payment_DuitNow_OBW extends Omise_Payment_Offsite {
-	public function __construct() {
+class Omise_Payment_DuitNow_OBW extends Omise_Payment_Offsite
+{
+	public function __construct()
+	{
 		parent::__construct();
 
 		$this->id                 = 'omise_duitnow_obw';
 		$this->has_fields         = true;
-		$this->method_title       = __( 'Omise DuitNow Online Banking/Wallets', 'omise' );
-		$this->method_description = __( 'Accept payment through <strong>DuitNow Online Banking/Wallets</strong> via Omise payment gateway.', 'omise' );
-		$this->supports           = array( 'products', 'refunds' );
+		$this->method_title       = __('Omise DuitNow Online Banking/Wallets', 'omise');
+		$this->method_description = __('Accept payment through <strong>DuitNow Online Banking/Wallets</strong> via Omise payment gateway.', 'omise');
+		$this->supports           = array('products', 'refunds');
 
 		$this->init_form_fields();
 		$this->init_settings();
 
-		$this->title                = $this->get_option( 'title' );
-		$this->description          = $this->get_option( 'description' );
-		$this->restricted_countries = array( 'MY' );
+		$this->title                = $this->get_option('title');
+		$this->description          = $this->get_option('description');
+		$this->restricted_countries = array('MY');
 		$this->source_type          = 'duitnow_obw';
 
-		add_action( 'woocommerce_api_' . $this->id . '_callback', 'Omise_Callback::execute' );
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-		add_action( 'woocommerce_order_action_' . $this->id . '_sync_payment', array( $this, 'sync_payment' ) );
+		add_action('woocommerce_api_' . $this->id . '_callback', 'Omise_Callback::execute');
+		add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+		add_action('woocommerce_order_action_' . $this->id . '_sync_payment', array($this, 'sync_payment'));
+		add_action('woocommerce_after_checkout_validation', array($this, 'check_bank_selected'), null, 2);
 	}
 
 	/**
 	 * @see WC_Settings_API::init_form_fields()
 	 * @see woocommerce/includes/abstracts/abstract-wc-settings-api.php
 	 */
-	public function init_form_fields() {
+	public function init_form_fields()
+	{
 		$this->form_fields = array(
 			'enabled' => array(
-				'title'   => __( 'Enable/Disable', 'omise' ),
+				'title'   => __('Enable/Disable', 'omise'),
 				'type'    => 'checkbox',
-				'label'   => __( 'Enable Omise DuitNow Online Banking/Wallets Payment', 'omise' ),
+				'label'   => __('Enable Omise DuitNow Online Banking/Wallets Payment', 'omise'),
 				'default' => 'no'
 			),
 
 			'title' => array(
-				'title'       => __( 'Title', 'omise' ),
+				'title'       => __('Title', 'omise'),
 				'type'        => 'text',
-				'description' => __( 'This controls the title the user sees during checkout.', 'omise' ),
-				'default'     => __( 'DuitNow Online Banking/Wallets', 'omise' ),
+				'description' => __('This controls the title the user sees during checkout.', 'omise'),
+				'default'     => __('DuitNow Online Banking/Wallets', 'omise'),
 			),
 
 			'description' => array(
-				'title'       => __( 'Description', 'omise' ),
+				'title'       => __('Description', 'omise'),
 				'type'        => 'textarea',
-				'description' => __( 'This controls the description the user sees during checkout.', 'omise' )
+				'description' => __('This controls the description the user sees during checkout.', 'omise')
 			),
 		);
 	}
 
-    /**
+	/**
 	 * @inheritdoc
 	 */
-	public function payment_fields() {
+	public function payment_fields()
+	{
 		parent::payment_fields();
 		$currency   = get_woocommerce_currency();
 		$cart_total = WC()->cart->total;
@@ -144,12 +149,13 @@ class Omise_Payment_DuitNow_OBW extends Omise_Payment_Offsite {
 	/**
 	 * @inheritdoc
 	 */
-	public function charge( $order_id, $order ) {
-		$source_bank	= isset( $_POST['source']['bank'] ) ? $_POST['source']['bank'] : '';
-		
+	public function charge($order_id, $order)
+	{
+		$source_bank	= isset($_POST['source']['bank']) ? $_POST['source']['bank'] : '';
+
 		$metadata = array_merge(
-			apply_filters( 'omise_charge_params_metadata', array(), $order ),
-			array( 'order_id' => $order_id ) // override order_id as a reference for webhook handlers.
+			apply_filters('omise_charge_params_metadata', array(), $order),
+			array('order_id' => $order_id) // override order_id as a reference for webhook handlers.
 		);
 
 		$return_uri = add_query_arg(
@@ -160,17 +166,17 @@ class Omise_Payment_DuitNow_OBW extends Omise_Payment_Offsite {
 			home_url()
 		);
 
-		return OmiseCharge::create( array(
-			'amount'      => Omise_Money::to_subunit( $order->get_total(), $order->get_currency() ),
+		return OmiseCharge::create(array(
+			'amount'      => Omise_Money::to_subunit($order->get_total(), $order->get_currency()),
 			'currency'    => $order->get_currency(),
-			'description' => apply_filters( 'omise_charge_params_description', 'WooCommerce Order id ' . $order_id, $order ),
+			'description' => apply_filters('omise_charge_params_description', 'WooCommerce Order id ' . $order_id, $order),
 			'source'      => array(
 				'type' => $this->source_type,
-				'bank' => sanitize_text_field( $source_bank ),
+				'bank' => sanitize_text_field($source_bank),
 			),
 			'return_uri'  => $return_uri,
 			'metadata'    => $metadata
-		) );
+		));
 	}
 
 	/**
@@ -178,11 +184,12 @@ class Omise_Payment_DuitNow_OBW extends Omise_Payment_Offsite {
 	 *
 	 * @see WC_Payment_Gateway::get_icon()
 	 */
-	public function get_icon() {
-		$icon = Omise_Image::get_image( array(
-			    'file' => 'duitnow-obw.png',
-			    'alternate_text' => 'DuitNow Online Banking/Wallets',
+	public function get_icon()
+	{
+		$icon = Omise_Image::get_image(array(
+			'file' => 'duitnow-obw.png',
+			'alternate_text' => 'DuitNow Online Banking/Wallets',
 		));
-		return apply_filters( 'woocommerce_gateway_icon', $icon, $this->id );
+		return apply_filters('woocommerce_gateway_icon', $icon, $this->id);
 	}
 }
