f4bb7a263b32bca6074a7a35b9dcba5d7291b356
Aashish <aashish@omise.co>
2022-10-27 10:17:17 +0545

    [ENGA3-518]: Fixed typo while requiring file.

--- includes/libraries/omise-plugin/Omise.php
+++ includes/libraries/omise-plugin/Omise.php
@@ -4,5 +4,5 @@ require_once dirname(__FILE__).'/helpers/charge.php';
 require_once dirname(__FILE__).'/helpers/wc_order.php';
 require_once dirname(__FILE__).'/helpers/mailer.php';
 require_once dirname(__FILE__).'/helpers/request.php';
-require_once dirname(__FILE__).'/helpers/Token.php';
+require_once dirname(__FILE__).'/helpers/token.php';
 require_once dirname(__FILE__).'/helpers/RedirectUrl.php';
