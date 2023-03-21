3c2a465cd0fcdf504d3cdc79ebd6346b183bce30
AJ <108650842+ajzkk@users.noreply.github.com>
2023-03-21 11:45:39 +0700

    remove scb and ktb internet banking (#356)

--- templates/payment/form-internetbanking.php
+++ templates/payment/form-internetbanking.php
@@ -1,29 +1,5 @@
 <fieldset id="omise-form-internetbanking">
 	<ul class="omise-banks-list">
-		<!-- SCB -->
-		<li class="item">
-			<input id="internet_banking_scb" type="radio" name="omise-offsite" value="internet_banking_scb" />
-			<label for="internet_banking_scb">
-				<div class="bank-logo scb"></div>
-				<div class="bank-label">
-					<span class="title"><?php _e( 'Siam Commercial Bank', 'omise' ); ?></span><br/>
-					<span class="omise-secondary-text"><?php _e( 'Fee: 15 THB (same zone), 30 THB (out zone)', 'omise' ); ?></span>
-				</div>
-			</label>
-		</li>
-
-		<!-- KTB -->
-		<li class="item">
-			<input id="internet_banking_ktb" type="radio" name="omise-offsite" value="internet_banking_ktb" />
-			<label for="internet_banking_ktb">
-				<div class="bank-logo ktb"></div>
-				<div class="bank-label">
-					<span class="title"><?php _e( 'Krungthai Bank', 'omise' ); ?></span><br/>
-					<span class="omise-secondary-text"><?php _e( 'Fee: 15 THB (same zone), 15 THB (out zone)', 'omise' ); ?></span>
-				</div>
-			</label>
-		</li>
-
 		<!-- BAY -->
 		<li class="item">
 			<input id="internet_banking_bay" type="radio" name="omise-offsite" value="internet_banking_bay" />
