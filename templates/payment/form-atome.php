371deb9fd4566d03bf67194b7914a1b4867b45c6
Aashish Gurung <101558497+aashishgurung@users.noreply.github.com>
2023-04-18 14:28:04 +0700

    [ENGA3-929]:  Fixed the issue of Trumoney phone number field not displaying. Added a message in the Atome. (#367)
    Co-authored-by: Aashish <aashish@omise.co>

--- templates/payment/form-atome.php
+++ templates/payment/form-atome.php
@@ -1,34 +1,29 @@
-<?php if($viewData['status']) : ?>
+<?php if ($viewData['status']) : ?>
     <fieldset id="omise-form-atome">
-        <?php _e( 'Atome phone number', 'omise' ); ?><br/>
+        <?php _e('Atome phone number', 'omise'); ?><br />
 
         <p class="form-row form-row-wide omise-label-inline">
             <input id="omise_atome_phone_default" type="checkbox" name="omise_atome_phone_default" value="1" checked="checked" />
-            <label for="omise_atome_phone_default"><?php _e( 'Same as Billing Detail', 'omise' ); ?></label>
+            <label for="omise_atome_phone_default"><?php _e('Same as Billing Detail', 'omise'); ?></label>
         </p>
 
         <p id="omise_atome_phone_field" class="form-row form-row-wide" style="display: none;">
             <span class="woocommerce-input-wrapper">
-                <input
-                    id="omise_atome_phone_number"
-                    class="input-text"
-                    name="omise_atome_phone_number"
-                    type="tel"
-                    autocomplete="off"
-                    placeholder="+66123456789"
-                />
+                <input id="omise_atome_phone_number" class="input-text" name="omise_atome_phone_number" type="tel" autocomplete="off" placeholder="+66123456789" />
             </span>
+        </p>
+
+        <p class="omise-secondary-text">
+            <?php _e('The phone number will be used for creating Atome charge', 'omise'); ?>
         </p>
     </fieldset>
 
     <script type="text/javascript">
-        var phone_number_field   = document.getElementById( 'omise_atome_phone_field' );
-        var phone_number_default = document.getElementById( 'omise_atome_phone_default' );
-
-        phone_number_default.addEventListener( 'change', ( e ) => {
-            phone_number_field.style.display = e.target.checked ? "none" : "block";
-        } );
+        document.getElementById('omise_atome_phone_default').addEventListener('change', (e) => {
+            const phone_number_field_atome = document.getElementById('omise_atome_phone_field')
+            phone_number_field_atome.style.display = e.target.checked ? "none" : "block";
+        });
     </script>
-<?php else: ?>
+<?php else : ?>
     <?php echo $viewData['message']; ?>
 <?php endif; ?>
