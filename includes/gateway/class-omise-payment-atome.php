f231df0b274dfcc683765611fd8cb94d36c14929
Aashish Gurung <101558497+aashishgurung@users.noreply.github.com>
2023-06-13 09:38:58 +0700

    Release v5.1.1 (#380)
    * [ENGA3-1334]: Fix invalid product issue with variance item (#377)
    
    * [ENGA3-1334]: Added WC_Product_Variation class when the item is a variant to fix the invalid prodcut issue. Atome logo is also added.
    
    * Fixed formatting.
    
    ---------
    
    Co-authored-by: Aashish <aashish@omise.co>
    
    * [ENGA3-1334]: Remove free products from the Atome bill (#378)
    
    * [ENGA3-1334]: Remove free products from Atome product list.
    
    * [ENGA3-1134]: Updated the message for Atome on freebie products.
    
    * [ENGA3-1134]: Check subtotal instead of price per item when comparing to zero.
    
    ---------
    
    Co-authored-by: Aashish <aashish@omise.co>
    
    * fixed not compatible with country based payments plugin (#379)
    
    * Preparing release of v5.1.1
    
    * Updated release date
    
    ---------
    
    Co-authored-by: Aashish <aashish@omise.co>
    Co-authored-by: (AJ) Zin Kyaw Kyaw <108650842+ajzkk@users.noreply.github.com>

--- includes/gateway/class-omise-payment-atome.php
+++ includes/gateway/class-omise-payment-atome.php
@@ -80,12 +80,12 @@ class Omise_Payment_Atome extends Omise_Payment_Offsite
     public function payment_fields()
     {
         parent::payment_fields();
-        $viewData = $this->validateMinRequiredAmount();
+        $viewData = $this->validateAtomeRequest();
 
         Omise_Util::render_view('templates/payment/form-atome.php', $viewData);
     }
 
-    private function validateMinRequiredAmount()
+    private function validateAtomeRequest()
     {
         $limits = [
             'thb' => [
@@ -103,7 +103,14 @@ class Omise_Payment_Atome extends Omise_Payment_Offsite
         ];
 
         $currency = strtolower(get_woocommerce_currency());
-        $cartTotal = WC()->cart->total;
+        $cart = WC()->cart;
+
+        if ($cart->subtotal === 0) {
+            return [
+                'status' => false,
+                'message' => 'Complimentary products cannot be billed.'
+            ];
+        }
 
         if (!isset($limits[$currency])) {
             return [
@@ -114,7 +121,7 @@ class Omise_Payment_Atome extends Omise_Payment_Offsite
 
         $limit = $limits[$currency];
 
-        if ($cartTotal < $limit['min']) {
+        if ($cart->total < $limit['min']) {
             return [
                 'status' => false,
                 'message' => sprintf(
@@ -125,7 +132,7 @@ class Omise_Payment_Atome extends Omise_Payment_Offsite
             ];
         }
 
-        if ($cartTotal > $limit['max']) {
+        if ($cart->total > $limit['max']) {
             return [
                 'status' => false,
                 'message' => __(
@@ -191,23 +198,44 @@ class Omise_Payment_Atome extends Omise_Payment_Offsite
 
         // Loop through ordered items
         foreach ($items as $key => $item) {
-            $product_variation_id = $item['variation_id'];
-
-            // Check if product has variation.
-            $productId = $product_variation_id ? $item['variation_id'] : $item['product_id'];
-            $product = new WC_Product($productId);
-            $sku = $product->get_sku();
             // item don't have price. So we have to take subtotal and divide it by quantity to get the price
             $pricePerItem = $item['subtotal'] / $item['qty'];
+
+            // Remove product from the list if the price is 0
+            if ((float)$item['subtotal'] === 0.00) {
+                continue;
+            }
+
+            // Check if product has variation.
+            $product = $item['variation_id'] ?
+                new WC_Product_Variation($item['variation_id'])
+                : new WC_Product($item['product_id']);
+
+            $sku = $product->get_sku();
 
             $products[$key] = [
                 'quantity' => $item['qty'],
                 'name' => $item['name'],
                 'amount' => Omise_Money::to_subunit($pricePerItem, $currency),
-                'sku' => empty($sku) ? $productId : $sku // if sku is not present then pass productId
+                'sku' => empty($sku) ? $product->get_id() : $sku // if sku is not present then pass productId
             ];
         }
 
         return $products;
     }
+
+    /**
+	 * Get icons
+	 *
+	 * @see WC_Payment_Gateway::get_icon()
+	 */
+	public function get_icon() {
+		$icon = Omise_Image::get_image([
+			'file' => 'atome.png',
+			'alternate_text' => 'Atome logo',
+			'width' => 20,
+			'height' => 20
+		]);
+		return apply_filters('woocommerce_gateway_icon', $icon, $this->id);
+	}
 }
