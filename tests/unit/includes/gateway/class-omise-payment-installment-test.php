f523da2e99f6655ef79a9a97bf886478f5bfd56d
Aashish <aashish@omise.co>
2023-08-03 18:20:19 +0700

    Added unit test for installment

--- tests/unit/includes/gateway/class-omise-payment-installment-test.php
+++ tests/unit/includes/gateway/class-omise-payment-installment-test.php
@@ -1,13 +1,16 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-
-require_once __DIR__ . '/../../class-omise-unit-test.php';
+use Mockery;
 
 class Omise_Payment_Installment_Test extends TestCase
 {
-	public static function setUpBeforeClass(): void
+	public function setUp(): void
     {
+        $offsite = Mockery::mock('overload:Omise_Payment_Offsite');
+        $offsite->shouldReceive('init_settings');
+        $offsite->shouldReceive('get_option');
+
         if (!function_exists('wc_get_order')) {
 			function wc_get_order($orderId) {
                 $class = new stdClass();
@@ -21,6 +24,7 @@ class Omise_Payment_Installment_Test extends TestCase
                 $class = new stdClass();
                 $class->cart = new stdClass();
                 $class->cart->total = 999999;
+                return $class;
 			}
 		}
 
@@ -29,7 +33,23 @@ class Omise_Payment_Installment_Test extends TestCase
             $wp->query_vars = ['order-pay' => 11];
         }
 
-		require_once __DIR__ . '/../../../includes/gateway/class-omise-payment-installation.php';
+        if (!function_exists('wp_kses')) {
+            function wp_kses() {}
+        }
+
+        if (!function_exists('add_action')) {
+            function add_action() {}
+        }
+
+		require_once __DIR__ . '/../../../../includes/gateway/class-omise-payment-installment.php';
+	}
+
+    /**
+	 * close mockery after test cases are done
+	 */
+	public function tearDown(): void
+	{
+		Mockery::close();
 	}
 
     /**
