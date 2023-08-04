8685ae480d9beab6048f0d0567e5a13e8ee08009
Aashish <aashish@omise.co>
2023-08-04 10:45:04 +0700

    Fix the tests after code chagne.

--- tests/unit/includes/gateway/class-omise-payment-installment-test.php
+++ tests/unit/includes/gateway/class-omise-payment-installment-test.php
@@ -39,10 +39,14 @@ class Omise_Payment_Installment_Test extends TestCase
     {
         // mocking built-in WooCommerce function
         if (!function_exists('wc_get_order')) {
-            function wc_get_order()
-            {
-                $class = new stdClass();
-                $class->total = 999999;
+            function wc_get_order() {
+                $class = new class {
+                    public $property;
+                
+                    public function get_total() { 
+                        return 999999;
+                    }
+                };
                 return $class;
             }
         }
@@ -65,8 +69,7 @@ class Omise_Payment_Installment_Test extends TestCase
     {
         // mocking WC() method
         if (!function_exists('WC')) {
-            function WC()
-            {
+            function WC() {
                 $class = new stdClass();
                 $class->cart = new stdClass();
                 $class->cart->total = 999999;
