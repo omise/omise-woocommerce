0d4f93a588db6f927bbb6d873c67a36dc81bbea0
Aashish <aashish@omise.co>
2023-09-18 11:44:13 +0700

    Changed the logo and added a test.

--- includes/gateway/class-omise-payment-ocbc-digital.php
+++ includes/gateway/class-omise-payment-ocbc-digital.php
@@ -77,10 +77,12 @@ class Omise_Payment_OCBC_Digital extends Omise_Payment_Offsite {
 	 *
 	 * @see WC_Payment_Gateway::get_icon()
 	 */
-	public function get_icon() {
+	public function get_icon()
+	{
 		$icon = Omise_Image::get_image([
 			'file' => 'ocbc-digital.png',
 			'alternate_text' => 'OCBC Digital',
+			'width' => 60,
 		]);
 		return apply_filters( 'woocommerce_gateway_icon', $icon, $this->id );
 	}
--- tests/unit/includes/gateway/class-omise-payment-ocbc-digital-test.php
+++ tests/unit/includes/gateway/class-omise-payment-ocbc-digital-test.php
@@ -10,6 +10,7 @@ class Omise_Payment_OCBC_Digital_Test extends Offsite_Test
     {
         parent::setUp();
         require_once __DIR__ . '/../../../../includes/gateway/class-omise-payment-ocbc-digital.php';
+        require_once __DIR__ . '/../../../../includes/classes/class-omise-image.php';
         $this->obj = new Omise_Payment_OCBC_Digital();
     }
 
@@ -44,8 +45,34 @@ class Omise_Payment_OCBC_Digital_Test extends Offsite_Test
         $this->assertEquals('Opn Payments OCBC Digital', $this->obj->method_title);
     }
 
+    /**
+     * @test
+     */
     public function supportsIsCorrect()
     {
         $this->assertEqualsCanonicalizing([ 'products', 'refunds' ], $this->obj->supports);
+    }
+
+    /**
+     * @test
+     */
+    public function getIcon()
+    {
+        // mocking WP built-in functions
+        if (!function_exists('plugins_url')) {
+            function plugins_url() {
+                return "http://localhost";
+            }
+        }
+
+        if (!function_exists('apply_filters')) {
+            function apply_filters() {
+                return "http://localhost/image.png";
+            }
+        }
+
+        $result = $this->obj->get_icon();
+
+        $this->assertEquals("http://localhost/image.png", $result);
     }
 }
