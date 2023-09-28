94c88f33a77b522050e6925f7e613a9adc815a4f
Aashish <aashish@omise.co>
2023-09-28 11:38:33 +0700

    Added instructions in thank you page for promptpay payment.

--- includes/gateway/class-omise-payment-promptpay.php
+++ includes/gateway/class-omise-payment-promptpay.php
@@ -132,11 +132,23 @@ class Omise_Payment_Promptpay extends Omise_Payment_Offline {
 
 		if ( 'view' === $context ) : ?>
 			<div id="omise-offline-additional-details" class="omise omise-additional-payment-details-box omise-promptpay-details" <?php echo 'email' === $context ? 'style="margin-bottom: 4em; text-align:center;"' : ''; ?>>
-				<p><?php echo __( 'Scan the QR code to pay', 'omise' ); ?></p>
+				<p><strong><?php echo __( 'Scan the QR code to pay', 'omise' ); ?></strong></p>
+
+				<div class="qr-alert-box">
+					<p><strong>Please follow the instructions below to make payment:</strong></p>
+					<ul class="align-left">
+						<li>Download the QR code or open your preferred bank app to scan it</li>
+						<li>Check that the payment details are correct</li>
+						<li>Import the QR code image into your bank app or scan the QR code with your bank app to pay.</li>
+						<li>Share the payment slip from your bank app to the seller</li>
+					</ul>
+				</div>
+
 				<div class="omise omise-promptpay-qrcode" alt="Opn Payments QR code ID: <?php echo $charge['source']['scannable_code']['image']['id']; ?>">
 					<?php $this->load_qr_svg_to_DOM($qrcode, 'omise-promptpay-qrcode-svg') ?>
 				</div>
 				<a id="omise-download-promptpay-qr" class="omise-download-promptpay-qr" href="<?php echo $qrcode ?>" download="qr_code.svg">Download QR</a>
+
 				<div>
 					<?php echo __( 'Payment expires in: ', 'omise' ); ?>
 					<?php echo wc_format_datetime( $expires_datetime, wc_date_format() ); ?>
