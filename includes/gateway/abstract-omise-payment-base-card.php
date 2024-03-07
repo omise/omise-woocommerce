2b4c7cdf87a5999ab171fd9880a8ac476769e34c
Aashish Gurung <101558497+aashishgurung@users.noreply.github.com>
2024-03-07 10:42:48 +0700

    Revert "Use wc order number metadata (#435)"
    This reverts commit a5354f3659f5522b8d4228f234668335db3ca9b9.

--- includes/gateway/abstract-omise-payment-base-card.php
+++ includes/gateway/abstract-omise-payment-base-card.php
@@ -62,14 +62,13 @@ abstract class Omise_Payment_Base_Card extends Omise_Payment
 	private function prepareChargeData($order_id, $order, $omise_customer_id, $card_id, $token)
 	{
 		$currency = $order->get_currency();
-		$order_number = $order->get_order_number();
 		$data = [
 			'amount' => Omise_Money::to_subunit($order->get_total(), $currency),
 			'currency' => $currency,
-			'description' => 'WooCommerce Order id ' . $order_number,
+			'description' => 'WooCommerce Order id ' . $order_id,
 			'return_uri' => $this->get_redirect_url('omise_callback', $order_id, $order),
 			'metadata' => $this->get_metadata(
-				$order_number,
+				$order_id,
 				[ 'secure_form_enabled' => $this->getSecureFormState()]
 			),
 		];
