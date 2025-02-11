893e483213c893a33afe38fa301b521d1d707653
Rossukhon Leagmongkol <rossukhon.l@opn.ooo>
2025-02-11 12:12:41 +0700

    [MIT-3134] Load Boost configurations

--- includes/blocks/assets/js/build/omise-one-click-apms.asset.php
+++ includes/blocks/assets/js/build/omise-one-click-apms.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'e6fac6c16b43d5aef4854d8cb63f068d');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'ecb6c787de7e3693ad79643e8e3ebcd8');
