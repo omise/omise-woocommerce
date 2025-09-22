736d8f332a0801de055ca2300f8cd934ce27b21e
Rossukhon Leagmongkol <rossukhon.l@opn.ooo>
2025-09-22 09:16:36 +0700

    [MIT-3446] Create passkey charge based on settings (#541)
    * [MIT-3446] Create charge with passkey when passkey is enabled
    
    * [MIT-3446] Renaming helper
    
    * [MIT-3446] Remove TODOs
    
    * [MIT-3446] Apply passkey flow on block only
    
    * [MIT-3446] Fix default value and update docs from feedbacks

--- includes/blocks/assets/js/build/credit_card.asset.php
+++ includes/blocks/assets/js/build/credit_card.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '7fa27c6b9484af754eb9616acb7e0efe');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'e259bc06a2c61674f41a149f58bf47c9');
--- tests/unit/Helpers/Omise_Setting_Helper.php
+++ tests/unit/Helpers/Omise_Setting_Helper.php
@@ -4,8 +4,16 @@ namespace Omise\Tests\Helpers;
 use Mockery;
 
 trait Omise_Setting_Helper {
-	// FIXME: Rename function name to snake case for consistency.
+	/**
+	 * @deprecated Use mock_omise_setting() instead.
+	 * This method is kept for backward compatibility and will be removed in the future.
+	 * FIXME: Refactor existing codebase to use snake case for consistency, then remove this function.
+	 */
 	protected function mockOmiseSetting( $pkey, $skey ) {
+		return $this->mock_omise_setting( $pkey, $skey );
+	}
+
+	protected function mock_omise_setting( $pkey, $skey ) {
 		$omise_setting_mock = Mockery::mock( 'alias:Omise_Setting' );
 
 		$omise_setting_mock->allows(
