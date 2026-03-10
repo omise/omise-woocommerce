dee2fcd548e95f0788f226a5bc4b361be3d9af1c
tyler-omise <sai.a@opn.ooo>
2026-03-10 17:35:21 +0700

    enhance test coverage

--- tests/unit/includes/omise-upa/stubs/payment-stubs.php
+++ tests/unit/includes/omise-upa/stubs/payment-stubs.php
@@ -5,13 +5,27 @@
  */
 
 if ( ! class_exists( 'Omise_Payment' ) ) {
-	class Omise_Payment {}
+	#[AllowDynamicProperties]
+	class Omise_Payment {
+		public $order;
+		public $method_title = '';
+		public $source_type  = '';
+		public $id           = '';
+
+		public function load_order( $order_id ) { return false; }
+		public function order() { return $this->order; }
+		public function process_payment( $order_id ) { return null; }
+		protected function invalid_order( $order_id ) { return array( 'result' => 'failure' ); }
+		protected function payment_failed( $charge, $reason = '' ) { return array( 'result' => 'failure' ); }
+	}
 }
 
 if ( ! class_exists( 'Omise_Payment_Offsite' ) ) {
+	#[AllowDynamicProperties]
 	class Omise_Payment_Offsite extends Omise_Payment {}
 }
 
 if ( ! class_exists( 'Omise_Payment_Offline' ) ) {
+	#[AllowDynamicProperties]
 	class Omise_Payment_Offline extends Omise_Payment {}
 }
